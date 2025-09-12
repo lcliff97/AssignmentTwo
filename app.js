0153c32c6652e4a67161193e007892fd4143b811
lcliff97 <louiecliff97@outlook.com>
2025-09-12 16:58:51 +0100

    more code

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const thumbnails = document.getElementById("thumbnails");
+
+const displayContainer = document.getElementById("displayContainer");
+
 const images = [
   {
     src: images / thailand.jpg,
@@ -12,3 +16,28 @@ const images = [
     alt: "Bukchon Hanok Village downtown Seoul",
   },
 ];
+
+function createThumbnails() {
+  images.array.forEach((element) => {
+    let imageElement = document.createElement("img");
+    console.log(`current image is`, thailand.jpg);
+
+    imageElement.src = image.src;
+
+    imageElement.addEventListener("click", function () {
+      console.log(thailand.jpg);
+      createBigImage(thailand.jpg);
+    });
+  });
+}
+
+createThumbnails();
+function createBigImage(imgDetails) {
+  displayContainer.innerHTML = "";
+  const bigImage = document.createElement("img");
+
+  bigImage.src = thailand.jpg;
+  bigImage.alt = "Monastaries on a Thai hillside.";
+
+  displayContainer.appendChild(bigImage);
+}
