eae5abaffae9ce496553f023abd45a2e79535204
lcliff97 <louiecliff97@outlook.com>
2025-09-12 16:11:49 +0100

    images

---
+++ app.js
@@ -0,0 +1,14 @@
+const images = [
+  {
+    src: images / thailand.jpg,
+    alt: "Monastaries on a Thai hillside.",
+  },
+  {
+    src: images / la.jpg,
+    alt: "Los Angeles skyline, palm tress in foregorund, skyscrapers in the distance",
+  },
+  {
+    src: images / korea.jpg,
+    alt: "Bukchon Hanok Village downtown Seoul",
+  },
+];
