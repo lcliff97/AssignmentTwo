74f09cb3e8904eb14330c150bbf3018860c5c9eb
lcliff97 <louiecliff97@outlook.com>
2025-09-14 14:33:36 +0100

    css

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
 const thumbnails = document.getElementById("thumbnails");
-
 const displayContainer = document.getElementById("displayContainer");
 
 const images = [
   {
-    src: images / thailand.jpg,
-    alt: "Monastaries on a Thai hillside.",
+    src: "images/thailand.jpg",
+    alt: "Monasteries on a Thai hillside.",
   },
   {
-    src: images / la.jpg,
-    alt: "Los Angeles skyline, palm tress in foregorund, skyscrapers in the distance",
+    src: "images/la.jpg",
+    alt: "Los Angeles skyline, palm trees in foreground, skyscrapers in the distance",
   },
   {
-    src: images / korea.jpg,
+    src: "images/korea.jpg",
     alt: "Bukchon Hanok Village downtown Seoul",
   },
 ];
 
 function createThumbnails() {
-  images.array.forEach((element) => {
+  images.forEach((element) => {
     let imageElement = document.createElement("img");
-    console.log(`current image is`, thailand.jpg);
-
-    imageElement.src = image.src;
+    imageElement.src = element.src;
+    imageElement.alt = element.alt;
+    imageElement.classList.add("thumb");
 
     imageElement.addEventListener("click", function () {
-      console.log(thailand.jpg);
-      createBigImage(thailand.jpg);
+      createBigImage(element);
     });
+
+    thumbnails.appendChild(imageElement);
   });
 }
 
-createThumbnails();
 function createBigImage(imgDetails) {
   displayContainer.innerHTML = "";
-  const bigImage = document.createElement("img");
 
-  bigImage.src = thailand.jpg;
-  bigImage.alt = "Monastaries on a Thai hillside.";
+  const bigImage = document.createElement("img");
+  bigImage.src = imgDetails.src;
+  bigImage.alt = imgDetails.alt;
+  bigImage.classList.add("big-image");
 
   displayContainer.appendChild(bigImage);
 }
