d8d9bf63e195862cc8bbdd5ceb2f432566ad9c96
lcliff97 <louiecliff97@outlook.com>
2025-09-14 15:24:41 +0100

    other images

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ const images = [
   },
 ];
 
+let currentIndex = 0;
+
 function createThumbnails() {
   images.forEach((element) => {
     let imageElement = document.createElement("img");
@@ -37,6 +39,29 @@ function createBigImage() {
   const bigImage = document.createElement("img");
   bigImage.src = "images/thailand.jpg";
   bigImage.alt = "Monasteries on a Thai hillside.";
+  bigImage.classList.add("big-image");
+
+  displayContainer.appendChild(bigImage);
+}
+
+function createBigImage() {
+  displayContainer.innerHTML = "";
+
+  const bigImage = document.createElement("img");
+  bigImage.src = "images/la.jpg";
+  bigImage.alt =
+    "Los Angeles skyline, palm trees in foreground, skyscrapers in the distance";
+  bigImage.classList.add("big-image");
+
+  displayContainer.appendChild(bigImage);
+}
+
+function createBigImage() {
+  displayContainer.innerHTML = "";
+
+  const bigImage = document.createElement("img");
+  bigImage.src = "images/korea.jpg";
+  bigImage.alt = "Bukchon Hanok Village downtown Seoul";
   bigImage.classList.add("big-image");
 
   displayContainer.appendChild(bigImage);
